3d39e6d34ce440e7757010080a9bee0bb231fe95
Juan Pablo Contreras Franco <juanp.contreras@gmail.com>
2023-02-26 08:06:26 +0100

    WIP Added development task refinement

--- include/Mempool.hpp
+++ include/Mempool.hpp
@@ -29,26 +29,54 @@ typedef std::vector<SubPoolDescriptor> MempoolConfig;
 
 /**
  * [ ] The mempool pre-allocates a continuous memory range that it will manage throughout its lifecycle.
+ *    [ ] Add OS system calls to allocate memory in Mempool constructor
+ *    [ ] Add OS system calls to free memory in Mempool constructor
+ *    [ ] Identify corner cases
+ *    [ ] Add test for corner cases
  * [ ] It is constructed from multiple, user-defined sub-pools with fixed memory chunk sizes.
  * [ ] The request to allocate memory within a mempool should result in an allocation of the
  *         chunk within the sub-pool that has the smallest possible chunk that will fit the
  *         requested allocation size.
- *
+ *    [ ] List corner cases to test
+ *    [ ] Add test for corner cases
+ *    [ ] Replace mocks in tests with gtest mocks.
  ** Requirements
  * [ ] Chunk sizes should be multiples of the provided alignment.
+  *   [ ] Identify corner cases
+ *    [ ] Add test for corner cases
  * [ ] The mempool has limited memory assigned when created should have an internal limit of the maximum amount of memory that it can allocate in total during creation.
 *  [x] Allocation of memory chunks should be protected against attempts to allocate a size bigger than the biggest chunk.
-// TODO [ ] Allocation of memory chunks should be protected against attempts to allocate more chunks that the given sub-pool can hold.
+ *    [ ] Identify more corner cases
+ *    [ ] Replace mocks in tests with gtest mocks.
+*  [ ] Allocation of memory chunks should be protected against attempts to allocate more chunks that the given sub-pool can hold.
+ *    [ ] Identify corner cases
+ *    [ ] Test corner cases
+ *    [ ] Replace mocks in tests with gtest mocks.
  * [ ] The creation of a mempool with sub-pool configuration where the sum of all chunks would be bigger than the predefined limit is disallowed.
  * [ ] Allocations & deallocations of memory chunks within the mempool should be thread safe.
+ *    [ ] Refactor data structures
+ *    [ ] Allow mutual exclusion through locks or other synchronization primitives
  * [ ] The mempool should operate only on the pre-allocated memory that was allocated during the mempool creation, therefore an attempt to allocate a chunk within the sub-pool that is fully allocated shouldn't cause the mempool to resize, but should be treated as an erroneous situation.
+ *    [ ] Test corner case: "every chunk in the chosen subpool is already allocated"
  * [ ] Deallocation of memory chunks should be protected against attempts to deallocate a pointer that wasn't allocated within the mempool.
+ *    [ ] free(void* p) shall throw an exception when the chunk freed is locked by mempool clients.
+ *    [ ] mempool shall throw an exception if a chunk cannot be freed because it is being read at least one chunk owner.
  * [ ] Allocation of the main memory range should be aligned to the provided alignment.
+ *    [ ] List corner cases to test
+ *    [ ] Add test for corner cases
  * [ ] Upon destruction of the mempool, the mempool should print the statistics of usage:
- *      - peak usage of the chunks within each sub-pool,
- *      - amount of unfreed chunks within each sub-pool.
- *      - Upon destruction of the mempool, the mempool should deallocate its underlying memory range.
+ *      [ ] peak usage of the chunks within each sub-pool,
+ *          [ ] Add access statistics by chunk
+ *      [ ] amount of unfreed chunks within each sub-pool.
+ *         [ ] Implement an option to enable full thread safety to allow destructing chunks that are still in use.
+ * [ ] Upon destruction of the mempool, the mempool should deallocate its underlying memory range.
+ *    [ ] Test corner case: "Chunks in use disallow destruction if are used by clients (only "when writing" and later "while being read and written" )"
+ *    [ ] List other corner cases to test
+ *    [ ] Add test for corner cases
  * [ ] Time complexity of allocation & deallocation should be linear or better
+ *    [ ] Replace vectors<> for a more efficient data structure (a tree? a hash table?).
+ *      [ ] Decide if boost/GLib/std or other library has datastructures with sublinear time complexity applicable for this problem.
+ *      [ ] Update CMakeLists.txt if necessary.
 */
 
 class Mempool {
